53336b505d7baa815cee101592585e97b802bcfe
sseung <sseutta@naver.com>
2024-12-20 13:29:51 +0900

    Initial commit

---
+++ bin/conduit_calculator.dart
@@ -0,0 +1,5 @@
+import 'package:conduit_calculator/conduit_calculator.dart' as conduit_calculator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${conduit_calculator.calculate()}!');
+}
