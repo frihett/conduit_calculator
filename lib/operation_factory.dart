12843af606c1c18db5009ffcaf730b526ad394b9
sseung <sseutta@naver.com>
2024-12-22 23:12:07 +0900

    feat: 단항, 이항 연산 클래스 생성 기능 추가

--- lib/operation_factory.dart
+++ lib/operation_factory.dart
@@ -1,7 +1,8 @@
-import 'package:conduit_calculator/operation.dart';
+import 'package:conduit_calculator/binary_operation.dart';
+import 'package:conduit_calculator/unary_operation.dart';
 
 class OperationFactory {
-  static Operation getOperation(String operator) {
+  static BinaryOperation getBinaryOperation(String operator) {
     switch (operator) {
       case '+':
         return Addition();
@@ -11,6 +12,13 @@ class OperationFactory {
         return Multiplication();
       case '/':
         return Division();
+      default:
+        throw FormatException('지원하지 않는 연산자입니다: $operator');
+    }
+  }
+
+  static UnaryOperation getUnaryOperation(String operator) {
+    switch (operator) {
       default:
         throw FormatException('지원하지 않는 연산자입니다: $operator');
     }
