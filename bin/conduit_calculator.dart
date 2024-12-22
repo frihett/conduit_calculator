f429ab8de9f8edc37f6d0ba20eacc6387dc165dd
sseung <sseutta@naver.com>
2024-12-22 16:48:10 +0900

    docs: 기본 파일 삭제

--- bin/conduit_calculator.dart
+++
@@ -1,5 +0,0 @@
-import 'package:conduit_calculator/conduit_calculator.dart' as conduit_calculator;
-
-void main(List<String> arguments) {
-  print('Hello world: ${conduit_calculator.calculate()}!');
-}
