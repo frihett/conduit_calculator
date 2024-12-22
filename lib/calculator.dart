97d98ff37eac362fb8551a814e44a7f4d50446b2
sseung <sseutta@naver.com>
2024-12-22 17:12:57 +0900

    refactor: 형식 검증 기능, 연산자 구분 기능 옮김
     operation.calculate();
     operation 코드 변경 되도 코드 수정 필요 없게 작성

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,33 +1,17 @@
+import 'package:conduit_calculator/input_parser.dart';
+import 'package:conduit_calculator/operation_factory.dart';
+
 class Calculator {
+  final inputParser = InputParser();
+
   double calculate(String input) {
-    final parts = input.split(' ');
-
-    if (parts.length != 3) {
-      throw FormatException('연산자 피연산자 연산자 형식을 맞춰주세요');
-    }
-
-    if (parts.any((part) => part.contains(','))) {
-      throw FormatException('천 단위 구분자는 허용되지 않습니다.');
-    }
+    final parts = inputParser.parse(input);
 
     final double operand1 = double.parse(parts[0]);
     final String operator = parts[1];
     final double operand2 = double.parse(parts[2]);
 
-    switch (operator) {
-      case '+':
-        return operand1 + operand2;
-      case '-':
-        return operand1 - operand2;
-      case '*':
-        return operand1 * operand2;
-      case '/':
-        if (operand2 == 0) {
-          throw ArgumentError('0으로 나눌수 없습니다.');
-        }
-        return operand1 / operand2;
-      default:
-        throw FormatException('지원하지 않는 연산자입니다 : $operator');
-    }
+    final operation = OperationFactory.getOperation(operator);
+    return operation.calculate(operand1, operand2);
   }
 }
