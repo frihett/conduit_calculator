d73bca33a227cd479940c44ae2f9a16627a5bddb
sseung <sseutta@naver.com>
2024-12-22 16:33:01 +0900

    test: 계산 테스트 수정
    calculator.calculate 수행

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -1,21 +1,30 @@
+import 'package:conduit_calculator/calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
+  final calculator = Calculator();
+
   group('계산 작업', () {
     test('더하기 테스트', () {
-      expect(Operator.add('3 + 2'), 5);
+      expect(calculator.calculate('3 + 2'), 5);
     });
 
     test('빼기 테스트', () {
-      expect(Operator.minus('5 - 3'), 2);
+      expect(calculator.calculate('5 - 3'), 2);
     });
 
     test('곱하기 테스트', () {
-      expect(Operator.multiply('4 * 2'), 8);
+      expect(calculator.calculate('4 * 2'), 8);
     });
 
-    test('나누기', () {
-      expect(Operator.divide('20 / 4'), 5);
+    test('나누기 테스트', () {
+      expect(calculator.calculate('20 / 4'), 5);
+    });
+    test('나누기 에러 테스트', () {
+      expect(
+        () => calculator.calculate('20 / 0'),
+        throwsArgumentError,
+      );
     });
   });
 }
