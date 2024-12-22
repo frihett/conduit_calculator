c5371e85c07716aedddb0323f24661dc25aacec4
sseung <sseutta@naver.com>
2024-12-22 23:11:35 +0900

    feat: 단항, 이항 연산자 구분 조건문 추가

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -7,11 +7,21 @@ class Calculator {
   double calculate(String input) {
     final parts = inputParser.parse(input);
 
-    final double operand1 = double.parse(parts[0]);
-    final String operator = parts[1];
-    final double operand2 = double.parse(parts[2]);
+    if (parts.length == 3) {
+      final double operand1 = double.parse(parts[0]);
+      final String operator = parts[1];
+      final double operand2 = double.parse(parts[2]);
 
-    final operation =  OperationFactory.getOperation(operator);
-    return operation.calculate(operand1, operand2);
+      final operation = OperationFactory.getBinaryOperation(operator);
+      return operation.calculate(operand1, operand2);
+    }
+    if (parts.length == 2) {
+      final operator = parts[0];
+      final double operand = double.parse(parts[1]);
+      final operation = OperationFactory.getUnaryOperation(operator);
+      return operation.calculate(operand);
+    } else {
+      throw FormatException('지원하지 않는 입력 형식입니다.');
+    }
   }
 }
